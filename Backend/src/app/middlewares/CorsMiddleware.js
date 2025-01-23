6e9ff982a225f55abff1e4379f3c931a22808510
Saullo-Programador <saullopaulodantasfelipe8@gmail.com>
2025-01-22 21:54:30 -0300

    update cors

--- Backend/src/app/middlewares/CorsMiddleware.js
+++ Backend/src/app/middlewares/CorsMiddleware.js
@@ -1,3 +1,11 @@
 const cors = require('cors');
 
+app.use(
+    cors({
+      origin: ['https://chat-bot-qo1lw6kb7-saullo-programadors-projects.vercel.app/'], // Substitua pela URL do frontend na Vercel
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true, // Se necessário (cookies, auth)
+    })
+  );
+
 module.exports = cors();

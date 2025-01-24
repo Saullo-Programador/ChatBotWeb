f3a13c9e2bcd29092a09c3a4012be88661b6d70f
Saullo-Programador <saullopaulodantasfelipe8@gmail.com>
2025-01-23 22:19:31 -0300

    atualizar

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,7 +4,6 @@ const whatsappClient = require('./infra/WhatsappClient');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }

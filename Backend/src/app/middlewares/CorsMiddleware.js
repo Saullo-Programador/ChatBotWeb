7a77eed0a47dcf2c7c85b255befb577cef025585
Saullo-Programador <saullopaulodantasfelipe8@gmail.com>
2025-01-23 21:48:44 -0300

    update cors

--- Backend/src/app/middlewares/CorsMiddleware.js
+++ Backend/src/app/middlewares/CorsMiddleware.js
@@ -1,3 +1,21 @@
 const cors = require('cors');
 
-module.exports = cors();
+const allowedOrigins = [
+  'https://chatbotweb-q157.onrender.com', // URL do backend
+  'http://localhost:3000', // Para desenvolvimento local
+  'https://chat-bot-711u1fc10-saullo-programadors-projects.vercel.app/', // Substitua pela URL final do frontend após o deploy
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Permite requisições sem origem (como ferramentas do Postman) ou valida a origem
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Permite cookies se necessário
+};
+
+module.exports = cors(corsOptions);

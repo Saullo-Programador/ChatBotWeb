831aa5aeb6fda3ef792f3c3d895f5ce94efa5ede
Saullo-Programador <saullopaulodantasfelipe8@gmail.com>
2025-02-02 21:53:25 -0300

    update cors

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { setupMiddlewares, setupRoutes } = require('./interfaces/routes/Index.js');
 const whatsappClient = require('./infra/WhatsappClient');
-const cors = require('./app/middlewares/CorsMiddleware.js');
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env || 4000 ;
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
@@ -22,7 +22,13 @@ app.use((err, req, res, next) => {
 
 // Inicializar o cliente do WhatsApp
 whatsappClient.initialize();
-app.use(cors);
+
+app.use(cors({
+    origin: 'https://chat-bot-web-nine.vercel.app', // Permite apenas o seu frontend
+    methods: ['GET', 'POST'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
+}));
+
 // Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
